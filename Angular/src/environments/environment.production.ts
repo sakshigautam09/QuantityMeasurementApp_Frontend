fe3afdd2021e4185b4ee9d339fc6b9b7b4e5fdd5
sakshigautam09 <122722826+sakshigautam09@users.noreply.github.com>
2026-04-18 10:02:10 +0530

    [SakshiGautam]Refractored the environment.ts

--- Angular/src/environments/environment.production.ts
+++ Angular/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBase: 'https://quantity-measurement-api.onrender.com/api/v1',
-  googleClientId: '621930458072-e1r85is40jvtcb3l1509se4m6f04ekb0.apps.googleusercontent.com'
+  googleClientId: ''
 };
